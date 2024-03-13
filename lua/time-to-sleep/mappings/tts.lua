e00b6ae4448567d34f8164806321caf0f699cd42
mana-byte <manaiki.laut@centrale-med.fr>
2024-03-13 22:48:55 +0100

    FIXED:Typos everywhere

--- lua/time-to-sleep/mappings/tts.lua
+++ lua/time-to-sleep/mappings/tts.lua
@@ -1,7 +1,7 @@
 local modules = {
-    tts = require("lua.time-to-sleep.chearup.tts"),
+    tts = require("time-to-sleep.chearup.tts"),
 }
 
-local config = require("lua.time-to-sleep.config.config")
+local config = require("time-to-sleep.config.config")
 modules.tts.boot_tts(config)
 vim.keymap.set('n', config.mappings.tts.stop, modules.tts.stop_tts)
