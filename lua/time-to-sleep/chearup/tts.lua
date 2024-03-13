9a21cee1dcb60cc0e66641f042f6d8f661c3907d
mana-byte <manaiki.laut@centrale-med.fr>
2024-03-13 22:50:28 +0100

    FIXED: Typo

--- lua/time-to-sleep/chearup/tts.lua
+++ lua/time-to-sleep/chearup/tts.lua
@@ -1,6 +1,6 @@
 local M = {}
 M.status_bar_assistant = require("time-to-sleep.chearup.status-bar-assistant")
-M.floating_buffer = require("lua.time-to-sleep.utils.floating-buffer")
+M.floating_buffer = require("time-to-sleep.utils.floating-buffer")
 M.lualine = require("lualine.lualine_intergration")
 M.close_tts = require("time-to-sleep.utils.buffer_functions").close_tts
 
